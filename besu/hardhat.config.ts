9dde0f8153fff4b9acd1cb51ef2d6692f27ea05a
AntonioClaudioSouza <antonio.claudio.souza@gmail.com>
2024-12-16 19:56:50 -0300

    added config for optimizer contract

--- besu/hardhat.config.ts
+++ besu/hardhat.config.ts
@@ -2,7 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+   version: "0.8.28",
+   settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  },
   networks: {
     besu: {
       url: "http://127.0.0.1:8545",
