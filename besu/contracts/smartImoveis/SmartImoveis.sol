64cbcb0f6078ce9cbc9c30afa1cc6bdad7bdcaf8
AntonioClaudioSouza <antonio.claudio.souza@gmail.com>
2024-12-16 19:55:07 -0300

    added functions for payment and termination of rentals

--- besu/contracts/smartImoveis/SmartImoveis.sol
+++ besu/contracts/smartImoveis/SmartImoveis.sol
@@ -31,7 +31,7 @@ contract SmartImoveis is ERC721URIStorage, ConfigBase {
     constructor(address _tokenAddress) ERC721("ImoveisNFT", "IMV") {
         token = IERC20(_tokenAddress);
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);        
-        _grantRole(ADMIN_ROLE, msg.sender);      
+        _grantRole(ADMIN_ROLE, msg.sender);   
     }
 
     function getImovel(uint256 _id) public view returns(ImoveisLibrary.Imovel memory){
@@ -58,9 +58,43 @@ contract SmartImoveis is ERC721URIStorage, ConfigBase {
     }
 
     function alugarImovel(uint256 _id) public onlyRole(LOCATARIO_ROLE){
-        require(imoveis[_id].id != 0, "Imovel nao existe");
         imoveis.alugarImovel(_id, msg.sender);
         alugueisDoLocatario[msg.sender].push(_id);
         emit Events.ImovelAlugado(_id, msg.sender);
+    }
+
+    function pagarAluguel(uint256 _id) public onlyRole(LOCATARIO_ROLE){
+        (uint256 valorPagto, uint256 taxaPlataforma) = imoveis.pagarAluguel(_id, config.taxaLocacao, msg.sender, config.adminAddress, token);
+        emit Events.AluguelPago(_id, msg.sender,valorPagto);
+        emit Events.TaxaPlataformaEnviada(_id, taxaPlataforma);
+    }
+
+    function solicitarEncerramentoLocatario(uint256 _id) public onlyRole(LOCATARIO_ROLE){
+        imoveis.solicitarEncerramentoLocatario(_id, msg.sender);
+        emit Events.SolicitacaoEncerramento(_id, msg.sender);
+    }
+
+    function solicitarEncerramentoProprietario(uint256 _id) public onlyRole(PROPRIETARIO_ROLE){
+        imoveis.solicitarEncerramentoProprietario(_id, msg.sender);
+        emit Events.SolicitacaoEncerramento(_id, msg.sender);
+    }
+
+    function realizarVistoria(uint256 _id, bool _aprovado) public onlyRole(VISTORIADOR_ROLE){
+        uint256 multa = imoveis.realizarVistoria(_id, _aprovado);
+        if(multa>0){
+            emit Events.MultaAplicada(_id, multa);
+        }      
+        emit Events.VistoriaConcluida(_id, _aprovado);
+    }
+
+    function pagarMulta(uint256 _id) public onlyRole(LOCATARIO_ROLE){
+        (uint256 multa, uint256 taxa) = imoveis.pagarMulta(_id, msg.sender, config.adminAddress, token);
+        emit Events.MultaPaga(_id, msg.sender, multa);
+        emit Events.TaxaPlataformaEnviada(_id, taxa);
+    }
+
+    function confirmarEncerramento(uint256 _id) public onlyRole(LOCATARIO_ROLE){
+        imoveis.confirmarEncerramento(_id, msg.sender);
+        emit Events.EncerramentoConfirmado(_id, msg.sender);
     }
 }
--- besu/contracts/smartImoveis/imoveisLibrary.sol
+++ besu/contracts/smartImoveis/imoveisLibrary.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./customMathLibrary.sol";
+
 /**
  * @title ImoveisLibrary
  * @dev Biblioteca de estrutura de imóveis
@@ -21,13 +24,15 @@ library ImoveisLibrary {
             bool isEncerramentoSolicitadoProprietario;  // Indica se o proprietário solicitou encerramento
             
             // Vistoria
+            bool isSolicitarVistoria;                   // Indica se a solicitação de vistoria do imovel
             bool isVistoriaConcluida;                   // Indica se a vistoria foi concluída
             bool isVistoriaAprovada;                    // Indica se a vistoria foi aprovada
             bool hasMultaPorVistoria;                   // Indica se há multa gerada pela vistoria
             bool isMultaPaga;                           // Indica se a multa foi paga
+            uint256 valorMultaVistoria;                 // Valor da multa gerada pela vistoria
         }
-
-
+    
+    
     /**
      * @dev Adiciona um novo imóvel ao contrato
      * @param _imoveis Mapeamento de imóveis
@@ -87,6 +92,170 @@ library ImoveisLibrary {
         imovel.locatario = _locatario;
         imovel.proximoVencimento = block.timestamp + 30 days;
         imovel.isDisponivelParaLocacao = false;
+        imovel.isEncerramentoSolicitadoLocatario = false;
+        imovel.isEncerramentoSolicitadoProprietario = false;
+        imovel.isVistoriaConcluida = false;
+        imovel.isVistoriaAprovada = false;
+        imovel.hasMultaPorVistoria = false;
+        imovel.isMultaPaga = false;
+        imovel.isSolicitarVistoria = false;
+        imovel.valorMultaVistoria = 0;
+    }
+  
+    /**
+     * @dev Paga o aluguel de um imóvel
+     * @param _imoveis Mapeamento de imóveis
+     * @param _id Identificador do imóvel
+     * @param _taxaLocacao Taxa de locação
+     * @param _pagador Endereço do pagador
+     * @param _plataforma Endereço da plataforma
+     * @param _token Contrato do token
+     */
+    function pagarAluguel(
+        mapping(uint256 => Imovel) storage _imoveis,
+        uint256 _id,
+        uint256 _taxaLocacao,
+        address _pagador,
+        address _plataforma,
+        IERC20 _token
+    ) internal returns(uint256,uint256) {
+        Imovel storage imovel = _imoveis[_id];
+        require(_imoveis[_id].id != 0, "Imovel nao existe");
+        require(imovel.locatario != address(0), "Imovel nao alugado");
+        require(block.timestamp <= imovel.proximoVencimento, "Aluguel ja venceu");
+
+        uint256 taxaPlataforma = CustomMath.calculateTax(imovel.valorLocacao, _taxaLocacao);
+        uint256 valorLiquido = CustomMath.calculateNetValue(imovel.valorLocacao,_taxaLocacao);
+
+        require(_token.balanceOf(_pagador) >= imovel.valorLocacao, "Saldo insuficiente");
+        require(_token.transferFrom(_pagador, _imoveis[_id].proprietario, valorLiquido), "Falha ao transferir o aluguel ao proprietario");
+        require(_token.transferFrom(_pagador, _plataforma, taxaPlataforma), "Falha ao transferir a taxa para a plataforma");
+        imovel.proximoVencimento += 30 days;
+        return ((taxaPlataforma + valorLiquido), taxaPlataforma);
+    }
+
+    /**
+     * @dev Solicita o encerramento de um imóvel pelo locatário
+     * @param _imoveis Mapeamento de imóveis
+     * @param _id Identificador do imóvel
+     * @param _locatario Endereço do locatário
+     */
+    function solicitarEncerramentoLocatario(
+        mapping(uint256 => Imovel) storage _imoveis,
+        uint256 _id,
+        address _locatario
+    ) internal {
+        Imovel storage imovel = _imoveis[_id];
+        require(imovel.id != 0, "Imovel nao existe");
+        require(imovel.locatario != address(0), "Imovel nao alugado");
+        require(imovel.locatario == _locatario, "Somente o locatario pode solicitar o encerramento");
+        imovel.isEncerramentoSolicitadoLocatario = true;
+        imovel.isVistoriaConcluida = false;
+        imovel.isSolicitarVistoria = true;
+    }
+
+    /**
+    * @dev Solicita o encerramento de um imóvel pelo proprietário
+    * @param _imoveis Mapeamento de imóveis
+    * @param _id Identificador do imóvel
+    * @param _proprietario Endereço do proprietário
+    */
+    function solicitarEncerramentoProprietario(
+        mapping(uint256 => Imovel) storage _imoveis,
+        uint256 _id,
+        address _proprietario
+    ) internal {
+        Imovel storage imovel = _imoveis[_id];
+        require(imovel.id != 0, "Imovel nao existe");
+        require(imovel.locatario != address(0), "Imovel nao alugado");
+        require(imovel.proprietario == _proprietario, "Somente o proprietario pode solicitar o encerramento");
+        imovel.isEncerramentoSolicitadoProprietario = true;
+        imovel.isVistoriaConcluida = false;
+        imovel.isSolicitarVistoria = true;
+    }
+
+    /**
+     * @dev Realiza a vistoria de um imóvel
+     * @param _imoveis Mapeamento de imóveis
+     * @param _id Identificador do imóvel
+     * @param _aprovado Indica se a vistoria foi aprovada
+     */
+    function realizarVistoria(
+        mapping(uint256 => Imovel) storage _imoveis,
+        uint256 _id,
+        bool _aprovado
+    ) internal returns(uint256) {
+        Imovel storage imovel = _imoveis[_id];
+        require(imovel.id != 0, "Imovel nao existe");
+        require(imovel.locatario != address(0), "Imovel nao alugado");
+        require(imovel.isEncerramentoSolicitadoLocatario || imovel.isEncerramentoSolicitadoProprietario, "Encerramento nao solicitado.");
+
+        imovel.isVistoriaConcluida = true;
+        imovel.isVistoriaAprovada = _aprovado;
+        imovel.valorMultaVistoria = 0;
+        
+        if (!_aprovado) {
+            imovel.valorMultaVistoria = CustomMath.calculateTax(imovel.valorLocacao, imovel.taxaMulta);
+            imovel.hasMultaPorVistoria = true; 
+            imovel.isMultaPaga = false;           
+        }
+        return imovel.valorMultaVistoria;
+    }
+
+    /**
+     * @dev Paga a multa de um imóvel
+     * @param _imoveis Mapeamento de imóveis
+     * @param _id Identificador do imóvel
+     * @param _pagador Endereço do pagador
+     * @param _token Contrato do token
+     */
+    function pagarMulta(
+        mapping(uint256 => Imovel) storage _imoveis,
+        uint256 _id,
+        address _pagador,
+        address _plataforma,
+        IERC20 _token
+    ) internal returns(uint256, uint256) {
+        Imovel storage imovel = _imoveis[_id];
+        require(imovel.id != 0, "Imovel nao existe");
+        require(imovel.locatario != address(0), "Imovel nao alugado");
+        require(imovel.locatario == _pagador, "Somente o locatario pode pagar a multa");
+        require(imovel.isVistoriaConcluida, "Vistoria nao concluida");
+        require(!imovel.isVistoriaAprovada, "Imovel passou na vistoria");
+        require(!imovel.isMultaPaga, "Multa ja foi paga");
+
+        uint256 multa = imovel.valorMultaVistoria;
+        uint256 taxa = CustomMath.calculateTax(imovel.valorMultaVistoria, imovel.taxaMulta);
+        uint256 valorLiquido = CustomMath.calculateNetValue(imovel.valorMultaVistoria, imovel.taxaMulta);
+
+        require(_token.balanceOf(_pagador) >= multa, "Saldo insuficiente");
+        require(_token.transferFrom(_pagador, imovel.proprietario, valorLiquido), "Falha ao transferir o valor da multa ao proprietario");
+        require(_token.transferFrom(_pagador, _plataforma, taxa), "Falha ao transferir a taxa para a plataforma");
+
+        imovel.isMultaPaga = true;
+        return (multa, taxa);
+    }
+
+    /**
+     * @dev Confirma o encerramento de contrato de locação de um imóvel
+     * @param _imoveis Mapeamento de imóveis
+     * @param _id Identificador do imóvel
+     */
+    function confirmarEncerramento(
+        mapping(uint256 => Imovel) storage _imoveis,
+        uint256 _id,
+        address _locatario
+    ) internal {
+        Imovel storage imovel = _imoveis[_id];
+        require(imovel.id != 0, "Imovel nao existe");
+        require(imovel.locatario != address(0), "Imovel nao alugado");
+        require(imovel.locatario == _locatario, "Somente o locatario pode confirmar o encerramento");
+        require(imovel.isVistoriaConcluida, "Vistoria nao concluida");
+        require(imovel.isVistoriaAprovada || imovel.isMultaPaga, "Multa nao paga ou vistoria nao concluida");
+
+        imovel.locatario = address(0);
+        imovel.proximoVencimento = 0;
+        imovel.isDisponivelParaLocacao = true;
         imovel.isEncerramentoSolicitadoLocatario = false;
         imovel.isEncerramentoSolicitadoProprietario = false;
         imovel.isVistoriaConcluida = false;
